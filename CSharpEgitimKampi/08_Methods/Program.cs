6fcb3c5f4326c7018e21fd0453bd67f131a034a3
Ömer Faruk Gündoğdu <omerrfarukgundogdu@gmail.com>
2025-02-06 11:40:56 +0300

    8 - 10 Numaralı Ders İçerikleri

---
+++ CSharpEgitimKampi/08_Methods/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Void Metotlar
+            //()
+            //Geriye Değer Döndürmeyen Metotlar
+            //void CustomerList()
+            //{
+            //    Console.WriteLine("Ali");
+            //    Console.WriteLine("Veli");
+            //    Console.WriteLine("Mehmet");
+            //    Console.WriteLine("Ahmet");
+            //    Console.WriteLine("Ayşe");
+            //}
+
+            //CustomerList();
+            #endregion
+
+            #region Geriye Değer Döndürmeyen Parametreli Metotlar
+            //void WriteMethod(string customerName)
+            //{
+            //    Console.WriteLine(customerName);
+            //}
+            //WriteMethod("Mehmet");
+            #endregion
+
+            #region Geriye Değer Döndürmeyen Int Parametreli Metotlar
+            //void Sum(int number1, int number2, int number3)
+            //{
+            //    int result = number1 + number2 + number3;
+            //    Console.WriteLine(result);
+            //}
+            //Sum(1,2,3);
+            #endregion
+
+            #region Geriye Değer Döndüren Metotlar
+            //string CustomerName()
+            //{
+            //    return "ÖMER";
+            //}
+            //Console.WriteLine(CustomerName());
+            #endregion
+
+            #region Geriye Değer Döndüren Parametreli Metotlar
+            //int Sum(int x,  int y) {
+            //return x + y;
+            //}
+
+            //Console.WriteLine(Sum(3, 4));
+            #endregion
+
+        }
+    }
+}
---
+++ CSharpEgitimKampi/10_DatabaseCRUD/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_DatabaseCRUD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("***** Menü Sipariş İşlem Paneli *****");
+            Console.WriteLine();
+
+            Console.WriteLine("--------------------------------");
+
+            #region Kategori Ekleme İşlemi
+            //Console.WriteLine("Eklemek İstediğiniz Kategori Adı: ");
+            //string categoryName = Console.ReadLine();
+
+            //SqlConnection connection = new SqlConnection("Data Source = DESKTOP-E9NIK52;initial Catalog = EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Insert Into TblCategory(CategoryName) Values (@p1)",connection);
+            //command.Parameters.AddWithValue("@p1", categoryName);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+            //Console.WriteLine("Kategori Başarıyla Eklendi!");
+            #endregion
+
+            #region Ürün Ekleme İşlemi
+            //string productName;
+            //decimal productPrice;
+            //bool productStatus;
+
+            //Console.Write("Ürün Adı: ");
+            //productName = Console.ReadLine();
+
+            //Console.Write("Ürün Fiyatı :");
+            //productPrice = decimal.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source = DESKTOP-E9NIK52;initial Catalog = EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Insert Into TblProduct(ProductName,ProductPrice,ProductStatus) Values(@productName,@productPrice,@productStatus)", connection);
+            //command.Parameters.AddWithValue("@productName", productName);
+            //command.Parameters.AddWithValue("@productPrice", productPrice);
+            //command.Parameters.AddWithValue("@productStatus", true);
+            //command.ExecuteNonQuery();
+            //connection.Close();
+
+            //Console.WriteLine("Ürün Ekleme İşlemi Başarılı!");
+            #endregion
+
+            #region Ürün Listeleme
+            //SqlConnection connection = new SqlConnection("Data Source = DESKTOP-E9NIK52;initial Catalog = EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Select * From TblProduct", connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(command);
+            //DataTable dataTable = new DataTable();
+            //adapter.Fill(dataTable);
+
+            //foreach (DataRow row in dataTable.Rows)
+            //{
+            //    var id = row["ProductId"];
+            //    var productName = row["ProductName"];
+            //    var productPrice = row["ProductPrice"];
+            //    var productStatus = row["ProductStatus"];
+
+
+            //    string cevap = (bool)productStatus ? " Var" : " Yok";
+
+            //    Console.Write($"{id}. {productName} Fiyat: {productPrice} ₺ Durumu :{cevap}");
+            //    Console.WriteLine();
+
+            //}
+            #endregion
+
+            #region Ürün Silme İşlemi
+
+            //Console.Write("Silinecek Ürün Id: ");
+            //int productId = int.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source = DESKTOP-E9NIK52;initial Catalog = EgitimKampiDb;integrated security=true");
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Delete From TblProduct Where ProductId=@productId", connection);
+            //command.Parameters.AddWithValue("@productId", productId);
+            //command.ExecuteNonQuery();
+
+            //connection.Close();
+
+            //Console.WriteLine("Silme İşlemi Başarıyla Gerçekleşti!");
+
+            #endregion
+
+            #region Ürün Güncelleme İşlemi
+
+            //Console.Write("Güncellenecek Ürün Id: ");
+            //int productId = int.Parse(Console.ReadLine());
+
+            //Console.Write("Güncellenecek Ürün Adı: ");
+            //string productName = Console.ReadLine();
+
+            //Console.Write("Güncellenecek Ürün Fiyatı: ");
+            //decimal productPrice = decimal.Parse(Console.ReadLine());
+
+            //SqlConnection connection = new SqlConnection("Data Source = DESKTOP-E9NIK52;initial Catalog = EgitimKampiDb;integrated security=true");
+
+            //connection.Open();
+            //SqlCommand command = new SqlCommand("Update TblProduct Set ProductName=@productName, ProductPrice=@productPrice Where ProductId=@productId", connection);
+            //command.Parameters.AddWithValue("@productName", productName);
+            //command.Parameters.AddWithValue("@productPrice", productPrice);
+            //command.Parameters.AddWithValue("@productId", productId);
+            //command.ExecuteNonQuery();
+
+            //connection.Close();
+
+            //Console.WriteLine("Ürün Bilgisi Başarıyla Güncellendi!");
+            #endregion
+        }
+    }
+}
